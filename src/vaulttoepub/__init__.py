33656c21c1324c77308ef79d9589a112e32a30b2
Ja Tar <tarnogorskijan@gmail.com>
2025-10-06 00:22:16 +0200

    testing, last fixes, whole ebook conv

--- src/vaulttoepub/__init__.py
+++ src/vaulttoepub/__init__.py
@@ -1,11 +1,17 @@
 import argparse
+from .vtoepub import generate_epub
+from pathlib import Path
 
 def get_version():
     return "0.1.0"
 
-def main():
-    print("VaultToEPUB version:", get_version())
+def main(input_dir: str, f_to_one_file: bool = False, output_file: str | None = None):
+    generate_epub(Path(input_dir), f_to_one_file, Path(output_file) if output_file else None)
 
 parser = argparse.ArgumentParser(description="VaultToEPUB command line interface")
-parser.add_argument("--version", action="version", version=get_version())
+parser.add_argument("input_dir", type=str, help="Path to the input directory containing markdown files")
+parser.add_argument("-f", "--folder_to_one_file", action="store_true", help="Option to turn lower folder with only .md to single file")
+parser.add_argument("-o", "--output_file", type=str, nargs='?', default=None, help="Path to the output EPUB file")
+parser.add_argument("-V", "--version", action="version", version=get_version())
 args = parser.parse_args()
+main(args.input_dir, args.folder_to_one_file, args.output_file)
--- src/vaulttoepub/vtoepub.py
+++ src/vaulttoepub/vtoepub.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import re
+import zipfile
 from pathlib import Path 
 from rich.logging import RichHandler
 from markdown_it import MarkdownIt
@@ -8,6 +9,7 @@ from bs4 import BeautifulSoup
 from bs4.element import Tag, NavigableString
 
 from .obsidian_classes import Header, BottomFootnote, InlineFootnote, SectionID, InLineLink
+from .files_classes import MDFile, CombinedMDFile
 
 logging.basicConfig(
     level="INFO",
@@ -105,7 +107,6 @@ class HTMLConverter:
                 log.info("Adding IDs to %d headers", len(headers))
                 for header in headers:
                     if isinstance(header, Tag):
-                        level = int(header.name[1])
                         text = header.get_text()
                         header_obj = Header(text)
                         header['id'] = header_obj.id
@@ -132,19 +133,21 @@ class HTMLConverter:
         def convert_inline_links(self):
             # Convert internal links like [[#^d758ad]] or [[Note Title]]
             input_soup = self.soup
+            text_nodes = input_soup.find_all(string=re.compile(InLineLink.regex))
 
-            for text_node in input_soup.find_all(string=re.compile(InLineLink.regex)):
+            for text_node in text_nodes:
                 if not isinstance(text_node, NavigableString):
                     continue
                 matches = re.findall(InLineLink.regex, str(text_node))
                 modified_text = str(text_node)
                 for match in matches:
                     link = InLineLink(str(match))
-                    log.info("Processing link: %s", link)
+                    log.debug("Processing link: %s", link)
                     modified_text = modified_text.replace(str(f"[[{match}]]"), str(link.to_html()))
                 
                 text_node.replace_with(BeautifulSoup(modified_text, "html.parser"))
                     
+            log.info("Converted %d inline links", len(text_nodes))
             self.soup = input_soup
 
 def normalize_file_name(file_path: Path) -> Path:
@@ -154,7 +157,7 @@ def normalize_file_name(file_path: Path) -> Path:
     normalized_name = normalized_name.strip('_')
     return file_path.with_name(normalized_name).with_suffix(file_path.suffix)
 
-def convert_file_to_xhtml(file_path: Path, save_dir: Path | None = None) -> Path:
+def convert_file_to_xhtml(file_path: Path, save_dir: Path | None = None) -> MDFile:
     md_conv = MDConverter(get_markdown_from_file(file_path))
     title = Header(md_conv.get_title(file_path))
     md_conv.remove_yaml_frontmatter()
@@ -164,10 +167,7 @@ def convert_file_to_xhtml(file_path: Path, save_dir: Path | None = None) -> Path
     html_content = html_conv.get_converted_html()
 
     # create XHTML file
-    new_file_path = file_path.with_suffix(".xhtml")
-    new_file_path = normalize_file_name(new_file_path)
-    if save_dir:
-        new_file_path = Path(save_dir) / new_file_path.name
+    new_file_path = generate_xhtml_path(file_path, save_dir)
 
     xhtml_content = (
         '<?xml version="1.0" encoding="UTF-8"?>\n'
@@ -178,7 +178,7 @@ def convert_file_to_xhtml(file_path: Path, save_dir: Path | None = None) -> Path
         f"  <title>{title.text}</title>\n"
         "</head>\n"
         "<body>\n"
-        '<section role="doc-chapter" epub:type="chapter" aria-labelledby="heading1">\n'
+        f'<section role="doc-chapter" epub:type="chapter" aria-labelledby="{title.id}">\n'
         f'<h1 id="{title.id}">{title.text}</h1>\n'
         f'{html_content}\n'
         "</section>\n"
@@ -186,24 +186,194 @@ def convert_file_to_xhtml(file_path: Path, save_dir: Path | None = None) -> Path
         "</html>"
     )
 
+    save_file(new_file_path, xhtml_content)
+
+    return MDFile(new_file_path)
+
+def generate_xhtml_path(file_path: Path, save_dir: Path | None = None) -> Path:
+    new_file_path = file_path.with_suffix(".xhtml")
+    new_file_path = normalize_file_name(new_file_path)
+    if save_dir:
+        new_file_path = Path(save_dir) / new_file_path.name
+    return new_file_path
+
+def save_file(new_file_path: Path, xhtml_content: str):
     with open(new_file_path, "w", encoding="utf-8") as f:
         f.write(xhtml_content)
 
-    return new_file_path
+def combine_files_to_xhtml(file_paths: list[Path], filename: str, save_dir: Path | None = None) -> CombinedMDFile:
+    title = Header(filename)
+    xhtml_content = (
+        '<?xml version="1.0" encoding="UTF-8"?>\n'
+        "<!DOCTYPE html>\n"
+        '<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops">\n'
+        "<head>\n"
+        '  <meta charset="utf-8"/>\n'
+        f"  <title>{title.text}</title>\n"
+        "</head>\n"
+        "<body>\n"
+        f'<h1 id="{title.id}">{title.text}</h1>\n'
+    )
 
-def convert_directory_to_xhtml(directory_path: Path):
-    tempdir: Path = directory_path.parent / f"{directory_path.name}_temp"
-    create_temp_directory(tempdir)
+    for _, file_path in enumerate(file_paths):
+        md_conv = MDConverter(get_markdown_from_file(file_path))
+        title = Header(md_conv.get_title(file_path)) # Every title is a header <h1> with section
+        md_conv.remove_yaml_frontmatter()
 
-    for root, _, files in os.walk(directory_path):
+        html_conv = HTMLConverter(md_conv.convert_markdown_to_html())
+        html_conv.add_obsidian_formatting()
+        html_content = html_conv.get_converted_html()
+
+        xhtml_content += (
+            f'<section role="doc-chapter" epub:type="chapter" aria-labelledby="{title.id}">\n'
+            f'<h1 id="{title.id}">{title.text}</h1>\n'
+            f'{html_content}\n'
+            "</section>\n"
+        )
+    
+    xhtml_content += "</body>\n</html>"
+
+    new_file_path = generate_xhtml_path(Path(filename).with_suffix(".xhtml"), save_dir)
+    save_file(new_file_path, xhtml_content)
+
+    return CombinedMDFile(new_file_path, [MDFile(fp) for fp in file_paths])
+
+def convert_directory_to_xhtml(directory_path: Path, tempdir: Path, f_to_one_file: bool) -> list[CombinedMDFile | MDFile]:
+    converted_files: list[CombinedMDFile | MDFile] = []
+
+    for root, dirs, files in os.walk(directory_path):
+        if not dirs and f_to_one_file:
+            combine_files = []
+            for file in files:
+                if file.endswith(".md"):
+                    file_path = Path(root) / file
+                    combine_files.append(file_path)
+            if combine_files:
+                log.info("Last folder detected, combining %s files into one", len(combine_files))
+                filename = Path(root).name
+                converted_files.append(combine_files_to_xhtml(combine_files, filename, save_dir=tempdir / "OEBPS" / "Text"))
+                continue
         for file in files:
             if file.endswith(".md"):
                 file_path = Path(root) / file
-                convert_file_to_xhtml(file_path, save_dir=tempdir)
+                converted_files.append(convert_file_to_xhtml(file_path, save_dir=tempdir / "OEBPS" / "Text"))
+    
+    return converted_files
 
-def create_temp_directory(temp_dir: Path):
-    if not os.path.exists(temp_dir):
-        os.makedirs(temp_dir)
+def create_epub_temp_directory(temp_dir: Path):
+    directories = ["", "META-INF", "OEBPS", "OEBPS/Text", "OEBPS/Styles", "OEBPS/Images"]
 
-if __name__ == "__main__":
-    convert_directory_to_xhtml(Path("tests/examples/"))
+    for directory in directories:
+        if not os.path.exists(temp_dir / directory):
+            os.makedirs(temp_dir / directory)
+
+def create_epub_config_files(temp_dir: Path, converted_files: list[CombinedMDFile | MDFile], custom_css_file: Path | None = None):
+    generate_mimetype_file(temp_dir)
+    generate_container_xml(temp_dir)
+    generate_default_css(temp_dir)
+    if custom_css_file:
+        # TODO Implement custom CSS handling
+        log.warning("Custom CSS handling is not implemented yet.")
+    generate_content_opf(temp_dir, converted_files, custom_css_file)
+
+def generate_mimetype_file(temp_dir: Path):
+    mimetype_path = temp_dir / "mimetype"
+    with open(mimetype_path, "w", encoding="utf-8") as f:
+        f.write("application/epub+zip")
+    
+    # Ensure the mimetype file is uncompressed
+    os.chmod(mimetype_path, 0o644)  # Set permissions to read/write for owner, read for others
+    log.info("Created mimetype file at: %s", mimetype_path)
+
+def generate_container_xml(temp_dir: Path):
+    container_path = temp_dir / "META-INF" / "container.xml"
+    container_content = (
+        '<?xml version="1.0" encoding="UTF-8"?>\n'
+        '<container version="1.0" xmlns="urn:oasis:names:tc:opendocument:xmlns:container">\n'
+        '  <rootfiles>\n'
+        '    <rootfile full-path="OEBPS/content.opf" media-type="application/oebps-package+xml"/>\n'
+        '  </rootfiles>\n'
+        '</container>'
+    )
+    
+    with open(container_path, "w", encoding="utf-8") as f:
+        f.write(container_content)
+    
+    log.info("Created container.xml at: %s", container_path)
+
+def generate_default_css(temp_dir: Path):
+    css_path = temp_dir / "OEBPS" / "Styles" / "default.css"
+    css_content = (
+        "body { font-family: Arial, sans-serif; line-height: 1.6; }\n"
+        "h1, h2, h3, h4, h5, h6 { font-weight: bold; }\n"
+        "p { margin-bottom: 1em; }\n"
+        "a { color: blue; text-decoration: underline; }\n"
+    )
+    
+    with open(css_path, "w", encoding="utf-8") as f:
+        f.write(css_content)
+    
+    log.info("Created default CSS file at: %s", css_path)
+
+def generate_content_opf(temp_dir: Path, converted_files: list[CombinedMDFile | MDFile], custom_css_file: Path | None = None):
+    content_opf_path = temp_dir / "OEBPS" / "content.opf"
+    
+    manifest_items: list[str] = []
+    spine_items: list[str] = []
+    #TODO Add handling for images and custom CSS
+    #TODO Add support for combined files
+    for _, md_file in enumerate(converted_files):
+        if isinstance(md_file, CombinedMDFile):
+            manifest_items.append(f'<item id="{md_file.name}" href="Text/{md_file.filename}" media-type="application/xhtml+xml" properties="rendition:flow-scrolled-doc"/>')
+            spine_items.append(f'<itemref idref="{md_file.name}"/>')
+        else:
+            manifest_items.append(f'<item id="{md_file.name}" href="Text/{md_file.filename}" media-type="application/xhtml+xml"/>')
+            spine_items.append(f'<itemref idref="{md_file.name}"/>')
+    
+    manifest = (
+        '<?xml version="1.0" encoding="utf-8"?>\n'
+        '<package version="3.0" unique-identifier="BookId" xmlns="http://www.idpf.org/2007/opf">\n'
+        '<metadata xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:opf="http://www.idpf.org/2007/opf">\n'
+        '<dc:identifier id="BookId">urn:uuid:780aec4a-test-ssss-ssss-658479f492e9</dc:identifier>\n' # TODO Generate unique UUID
+        '<dc:language>en</dc:language>\n' # TODO Allow setting language
+        '<dc:title>[Main title]</dc:title>\n' # TODO Allow setting title
+        '</metadata>\n'
+        '<manifest>\n'
+        f'{"\n".join(manifest_items)}\n'
+        '<item id="css" href="Styles/default.css" media-type="text/css"/>\n'
+        '</manifest>\n'
+        '<spine>\n'
+        f'{"\n".join(spine_items)}\n'
+        '</spine>\n'
+        '</package>'
+    )
+
+    save_file(content_opf_path, manifest)
+    log.info("Created content.opf at: %s", content_opf_path)
+
+def generate_epub(directory_path: Path, f_to_one_file: bool, output_path: Path | None = None):
+    tempdir: Path = directory_path.parent / f"{directory_path.name}_temp"
+    create_epub_temp_directory(tempdir)
+    converted_files = convert_directory_to_xhtml(directory_path, tempdir, f_to_one_file)
+    create_epub_config_files(tempdir, converted_files)
+
+    if output_path is None:
+        output_path = directory_path.parent / f"{directory_path.name}.epub"
+    
+    generate_epub_file(tempdir, output_path)
+
+def generate_epub_file(temp_dir: Path, output_path: Path):
+    # Create the final EPUB file from the temp directory
+    
+    with zipfile.ZipFile(output_path, 'w', zipfile.ZIP_DEFLATED) as epub:
+        # The mimetype file must be the first file and must not be compressed
+        mimetype_path = temp_dir / "mimetype"
+        epub.write(mimetype_path, 'mimetype', compress_type=zipfile.ZIP_STORED)
+        
+        for foldername, subfolders, filenames in os.walk(temp_dir):
+            for filename in filenames:
+                if filename == "mimetype":
+                    continue  # Already added
+                file_path = os.path.join(foldername, filename)
+                arcname = os.path.relpath(file_path, temp_dir)
+                epub.write(file_path, arcname)
